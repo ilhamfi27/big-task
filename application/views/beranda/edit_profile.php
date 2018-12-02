e5ed30d5edb5f38642db4d7ccd261d9fda07aa6b
sherli yualinda <42725403+sherliyualinda@users.noreply.github.com>
2018-12-02 22:32:47 +0700

    ini3

--- application/views/beranda/edit_profile.php
+++ application/views/beranda/edit_profile.php
@@ -32,10 +32,6 @@
 							<nav class="collapse navbar-collapse" role="navigation">
 							<form class="navbar-form navbar-left">
 								<div class="input-group input-group-sm" style="max-width:360px;">
-								  <input class="form-control" placeholder="Search" name="srch-term" id="srch-term" type="text">
-								  <div class="input-group-btn">
-									<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
-								  </div>
 								</div>
 							</form>
 							<ul class="nav navbar-nav navbar-right">
@@ -57,12 +53,6 @@
 									<div class="panel-body">
 										<center><h3>Profil Anda</h3></center>
 										<table class="table">
-											<tr>
-												<td></td>
-												<td>
-													<img src="1.png" width="100">
-												</td>
-											</tr>
 											<tr>
 												<td class="col-md-4"><label>Nama</label></td>
 												<td class="col-md-8">
@@ -109,6 +99,10 @@
 												</td>
 											</tr>
 											<tr>
+												<td></td>
+											</tr>
+											<tr>
+												<td></td>
 												<td>
 													<input type="submit" name="submit" value="submit">
 												</td>
